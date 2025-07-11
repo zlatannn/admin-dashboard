fdfe9a0af743ec70e71197fd29ef71ceff469b54
zlatannn <fanncia99@gmail.com>
2025-07-11 16:51:07 +0700

    update 16: signup update

---
+++ signup/signup.js
@@ -0,0 +1,13 @@
+document.getElementById("signup-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  const username = document.getElementById("new-username").value.trim();
+  const password = document.getElementById("new-password").value.trim();
+
+  if (username && password) {
+    localStorage.setItem("loggedInUser", username);
+    alert("Đăng ký thành công!");
+    window.location.href = "../landing_page/landing.html";
+  } else {
+    alert("Vui lòng nhập đủ thông tin!");
+  }
+});
